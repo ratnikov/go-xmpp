18a0ba4183dc4047edc69e352f42fbafe0efb467
Dmitry Ratnikov <ratnikov@google.com>
2011-03-08 17:25:54 -0500

    Finally fixed the makefile to build main correctly against the xmpp package.

--- Makefile
+++ Makefile
@@ -3,10 +3,8 @@ include $(GOROOT)/src/Make.inc
 TARG=xmpp
 GOFILES=auth.go connection.go
 
-%.6: %.go
-	6g -o $@ $<
-
 include $(GOROOT)/src/Make.pkg $<
 
-%.out: %.6
-	6l -o $@ $<
+main: package
+	6g -I_obj/ -o main.6 main.go
+	6l -L_obj/ -o main.out main.6
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "xmpp"
+)
 
 func main() {
+  client, err := xmpp.NewClient("ratnikov@gmail.com", "secret")
+
+  if err != nil {
+    fmt.Printf("Failed due to: %s\n", err)
+  }
+
   fmt.Printf("Hello world!\n")
+
+  fmt.Printf("Client: %x\n", client)
 }
