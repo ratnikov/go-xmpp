2b988b82b58ff403e09d2240efc99ac5c3e17266
Dmitry Ratnikov <ratnikov@google.com>
2011-03-22 22:25:24 -0400

    Extracted testing utils in a separate file.

--- auth_test.go
+++ auth_test.go
@@ -1,7 +1,6 @@
 package xmpp;
 
 import (
-  "regexp"
   "testing"
 )
 
@@ -21,30 +20,4 @@ func TestNewAuth(t *testing.T) {
   assertPanic(t, "Should complain about lack of @", func() {
     NewAuth("joe-someone.com", "secret")
   })
-}
-
-func assertEqual(t *testing.T, expected, actual interface{}, message string) {
-  if expected != actual {
-    t.Errorf("Expected <%s> but got <%s>", expected, actual)
-  }
-}
-
-func assertMatch(t *testing.T, regex_str, str, message string) {
-  regex := regexp.MustCompile(regex_str)
-
-  if !regex.MatchString(str) {
-    t.Errorf("%s: Expected <%s> to match <%s>", message, str, regex)
-  } else {
-    // all good
-  }
-}
-
-func assertPanic(t *testing.T, message string, f func()) {
-  defer func() {
-    if err := recover(); err == nil {
-      t.Errorf(message)
-    }
-  }()
-
-  f()
 }
