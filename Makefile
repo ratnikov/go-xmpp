4719fda693185a5bf2c1ab1a2b0da46ba7fb21e1
Dmitry Ratnikov <ratnikov@google.com>
2011-03-22 22:23:11 -0400

    Fixed the auth_test

--- Makefile
+++ Makefile
@@ -3,27 +3,14 @@ include $(GOROOT)/src/Make.inc
 TARG=xmpp
 GOFILES=auth.go connection.go util.go
 
-net.a:
-	cd net && gomake
-
-tls.a: net.a
-	cd tls && gomake
-
-tls-clean:
-	cd tls && gomake clean
-
-_xmpp_: tls.a
+_xmpp_:
 	6g -o _xmpp_.6 -Itls/_obj/ $(GOFILES)
-
-_obj:
-	mkdir _obj
 
 xmpp.a: _obj _xmpp_
 	gopack grc _obj/xmpp.a _xmpp_.6
 
 main: xmpp.a
-	6g -I_obj/ -Itls/_obj/ -o main.6 main.go
-	6l -L_obj/ -Ltls/_obj/ -o main.out main.6
+	6g -I_obj/ -o main.6 main.go
+	6l -L_obj/ -o main.out main.6
 
-clean: tls-clean
-	rm -rf *out _obj *6
+include $(GOROOT)/src/Make.pkg
--- auth_test.go
+++ auth_test.go
@@ -1,29 +1,50 @@
 package xmpp;
 
 import (
+  "regexp"
   "testing"
 )
 
 func TestNewAuth(t *testing.T) {
-  if auth, err := NewAuth("joe@test.com", "secret"); auth != nil {
+  if auth := NewAuth("joe@test.com", "secret"); auth != nil {
     assertEqual(t, "joe", auth.user, "Should parse the user")
     assertEqual(t, "test.com", auth.domain, "Should parse the domain")
     assertEqual(t, "secret", auth.password, "Should assign password")
   } else {
-    t.Errorf("Expected to parse %s as valid authentication info, but got nil. Accompanying error: %s", "joe@test.com", err)
+    t.Errorf("Expected to parse %s as valid authentication info, but got nil.")
   }
 
-  if auth, err := NewAuth("joe@johnson@test.com", "secret"); auth != nil || err == nil {
-    t.Error("Should not parse auth information with multiple '@'")
-  }
+  assertPanic(t, "Should not parse auth information with multiple '@'", func() {
+    NewAuth("joe@johnson@test.com", "secret")
+  })
 
-  if auth, err := NewAuth("joe-someone.com", "secret"); auth != nil || err == nil {
-    t.Error("Should complain about lack of @")
-  }
+  assertPanic(t, "Should complain about lack of @", func() {
+    NewAuth("joe-someone.com", "secret")
+  })
 }
 
 func assertEqual(t *testing.T, expected, actual interface{}, message string) {
   if expected != actual {
     t.Errorf("Expected <%s> but got <%s>", expected, actual)
   }
+}
+
+func assertMatch(t *testing.T, regex_str, str, message string) {
+  regex := regexp.MustCompile(regex_str)
+
+  if !regex.MatchString(str) {
+    t.Errorf("%s: Expected <%s> to match <%s>", message, str, regex)
+  } else {
+    // all good
+  }
+}
+
+func assertPanic(t *testing.T, message string, f func()) {
+  defer func() {
+    if err := recover(); err == nil {
+      t.Errorf(message)
+    }
+  }()
+
+  f()
 }
